c61bfc0e8bbd8b1609bba40c90d4e0d627fe8768
Anirudh <aniswami97@gmail.com>
2016-06-13 18:16:50 +0530

    Commented all the files

--- Common_Task2/dinogoogle.js
+++ Common_Task2/dinogoogle.js
@@ -1,15 +1,28 @@
-//alert('Hi');
 //All variables are declared here.
+
+//The components of the game
 var dinosaur,reload,gameOver;
+
+//The threshold for jumping
 var jump = 0;
+
+//Array of obstacles
 var cactii = [];
+
+//Randomize position of the obstacles
 var randPos = 0;
 var randBird = 70;
+
+//The moving background
 var bg,bg1;
+
+//The score components
 var score = document.getElementById("scores");
 var tot=0;
 var hiscore = 0;
 var msg = "SCORE: ";
+
+//Accelerate as score progresses
 var accel = 0,added = 0,fram = 145,fra1 = 80;
 
 //variables to add sound to the game
@@ -27,7 +40,7 @@ var paused =false;
 //Picture sources
 var dijum,biup,bidn;
 
-
+//Function to obtain the highscore from sessionStorage
 function getHi(){
 	if(sessionStorage.getItem("hiScore")!==null){
 		hiscore = parseInt(sessionStorage.getItem("hiScore"));
@@ -35,10 +48,15 @@ function getHi(){
 	}
 }
 
+//Starts the game
 function startGame(){
+	//Create and set the canvas
 	Anigame.start();
+	
+	//Get the high score
 	getHi();
 	
+	//Initialize the image sources, and insert dinosaur.
 	dinosaur = new component(100,75,"dino_jump.png",55,162,"image");
 	dijum = dinosaur.imag.src;
 	
@@ -46,31 +64,34 @@ function startGame(){
 	bidn = birdDo.imag.src;
 	var birdU = new component(50,24,"birdUp.png",600,190,"image");
 	biup = birdU.imag.src;
-	//alert(''+dijum);
 	bg = new component(600,270,"dino_bg.png",0,0,"background");
 	bg1 = new component(600,270,"dino_bg.png",600,0,"background");
 	
 	reload = new component(70,70,"reload.png",260,110,"image");
 	gameOver = new component(180,100,"gameOver.png",210,30,"image");
 	
+	//Initialize sounds
 	jumpSound = new playSound("dino_jump.mp3");
 	hitSound = new playSound("dino_hit.mp3");
 	highSound = new playSound("dino_high.mp3");
 }
 
+//The canvas object.
 var Anigame = {
 	canvas: document.createElement("canvas"),
 	start: function(){
 		this.canvas.width = 600;
 		this.canvas.height = 270;
 		this.context = this.canvas.getContext('2d');
-		//document.body.insertBefore(this.canvas,document.body.childNodes[0]);
 		document.body.appendChild(this.canvas);
 		this.context.fillStyle = "#f2f2f2";
 		this.context.fillRect(0,0,600,270);
 		this.frameNo = 0;
-		//FPS is (50)NO. It is now 125.
+		
+		//FPS is now 125.
 		this.interval = setInterval(updateArena,8);
+		
+		//Add eventlisteners for keydown, and touchstart
 		window.addEventListener('keydown',function(e){
 			Anigame.key = e.keyCode;
 			if(!started){
@@ -86,11 +107,14 @@ var Anigame = {
 		window.addEventListener('touchend',function(e){
 			Anigame.key = false;
 		})
-		//alert('Created canvas');
 	},
+	
+	//Clear the canvas
 	clear: function(){
 		this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
 	},
+	
+	//Stop the game
 	stop: function(){
 		clearInterval(this.interval);
 		if(Math.floor(tot/15) > hiscore) hiscore = Math.floor(tot/15);
@@ -106,6 +130,7 @@ var Anigame = {
 	}
 }
 
+//Create the components of the game
 function component(width,height,color,x,y,type){
 	this.type = type;
 	if(type == "image" || type=="background"){
@@ -118,6 +143,7 @@ function component(width,height,color,x,y,type){
 	this.x = x;
 	this.y = y;
 	
+	//Update the components
 	this.update = function(){
 		ctx = Anigame.context;
 		if(type == "image" || type == "background"){
@@ -129,6 +155,8 @@ function component(width,height,color,x,y,type){
 			ctx.fillRect(this.x,this.y,this.width,this.height);
 		}
 	}
+	
+	//Move to a new position
 	this.newPos = function(){
 		this.x+=this.speedX;
 		this.y+=this.speedY;
@@ -138,12 +166,13 @@ function component(width,height,color,x,y,type){
 			}
 		}
 	}
+	
+	//Check for crashes with obstacles
 	this.crashWith = function(cactii) {
         var dinol = this.x + 35;
         var dinor = this.x + (this.width)-23;
 		if(this.imag.src === dijum) {
 			dinor = this.x+ this.width - 35;
-			//alert('Jumping dino');
 		}
         //var dinotop = this.y;
         var dinobot = this.y + (this.height) - 18;
@@ -162,6 +191,8 @@ function component(width,height,color,x,y,type){
         }
         return crash;
     }
+	
+	//Changes the image source. Useful for animation
 	this.changeSrc = function(srcIt){
 		if(this.type === "image"){
 			this.imag.src = srcIt;
@@ -171,9 +202,14 @@ function component(width,height,color,x,y,type){
 
 
 
+//Update the game area. Basically called every 8 milliseconds
 function updateArena(){
+	
+	//If the game is not paused and has started
 	if(!paused && started){
 		var x,y;
+		
+		//Check for crashes
 		for(var i=0; i<cactii.length; ++i){
 			if(dinosaur.crashWith(cactii[i])){
 				hitSound.play();
@@ -189,6 +225,8 @@ function updateArena(){
 	
 		Anigame.frameNo+=1;
 		tot++;
+		
+		//Insert the obstacles at random intervals
 		if(Anigame.frameNo >= fram+randPos){
 			x = Anigame.canvas.width;
 			y = Anigame.canvas.height - 84;
@@ -199,14 +237,16 @@ function updateArena(){
 				cactii.push(bird);
 				randBird = randBird + Math.floor(Math.random()*85);
 			}
+			
+			//Randomly insert a bird
 			else {
 				cactii.push(new component(15,34,"cactus1.png",x,y,"image"));
-			//alert('randBird is '+randBird+' score is '+Math.floor(tot/15)+'score % randBird is '+Math.floor(tot/15)%randBird);
 			}
 			randPos = Math.floor(Math.random()*fra1);
 			Anigame.frameNo = 0;
 		}
 	
+		//Accelerate once the score reaches a threshold.
 		if(Math.floor(tot/15)!=0 && Math.floor(tot/15)%75 === 0){
 			if(added ==0){
 				accel+=0.1;
@@ -220,49 +260,55 @@ function updateArena(){
 	
 		dinosaur.speedX=0;
 		dinosaur.speedY = 0;
-
+		
+		//If the key pressed is a spacebar
 		if(Anigame.key && Anigame.key == 32){
-	//dinosaur.speedX = 0;
-	//dinosaur.speedY = -1;
 			if(jump == 0) {
 				jump = 120;
+				
+				//Change the image to show a jumping dino
 				dinosaur.changeSrc("dino_jump.png");
-			//dinosaur.changeSrc("dino.png");
 				jumpSound.play();
 			}
 		}
+		
+		//If not reached top, go up
 		if(jump>75) {
 			dinosaur.speedX = 0;
 			dinosaur.speedY = -2;
 			dinosaur.newPos();
 			jump--;
 		}
+		
+		//Stay at the top 
 		else if(jump>45){
 			dinosaur.speedX = 0;
 			dinosaur.speedY = 0;
 			dinosaur.newPos();
 			jump--;
 		}
+		
+		//Come down again to same position
 		else if(jump>0){
 			dinosaur.speedX = 0;
 			dinosaur.speedY = 2;
 			dinosaur.newPos();
 			jump--;
 		}
-	//cactus.x+=-1;
-	//cactus.update();
 		bg.speedX = -1-accel;
 		bg.speedY = 0;
 		bg1.speedX = -1-accel;
 		bg1.speedY = 0;
+		
+		//Update the positions of the background
 		bg.newPos();
 		bg.update();
 		bg1.newPos();
 		bg1.update();
-		Anigame.context.fillStyle = "#f7f7f7";//#f0f0f0 #f8f8f8
+		Anigame.context.fillStyle = "#f7f7f7";
 		Anigame.context.fillRect(55,172,100,50);
 	
-	//Sprite coding
+		//Sprite coding
 		if(jump == 0){
 			if(fra<10){
 				dinosaur.changeSrc("dino_right.png");
@@ -283,14 +329,16 @@ function updateArena(){
 			Anigame.context.stroke();	
 		}
 	
+		//Update each obstacle
 		for(var i=0; i<cactii.length; ++i){
 			cactii[i].x+=-1-accel;
 			if(cactii[i].vir<30){
+				
+				//If the obstacle is a bird,make it flap its wings
 				if(cactii[i].imag.src === biup||
 				cactii[i].imag.src === bidn){
 					cactii[i].changeSrc("birdDown.png");
 					cactii[i].vir++;
-				//alert('Changed source to dino.vir is '+cactii[i].vir);
 				}
 			}
 			else if(cactii[i].vir<60){
@@ -298,13 +346,13 @@ function updateArena(){
 				cactii[i].imag.src === biup){
 					cactii[i].changeSrc("birdUp.png");
 					cactii[i].vir++;
-				//alert('Changed source to Screenshot.vir is '+cactii[i].vir);
 				}		
 			}
 			else cactii[i].vir = 0;
 			cactii[i].update();
 		}
 	
+		//Update the score
 		score.innerHTML = msg+Math.floor(tot/15);
 		if(Math.floor(tot/15) == hiscore && Math.floor(tot/15)!==0) highSound.play();
 		if(Math.floor(tot/15)%100 == 0 && Math.floor(tot/15)!==0) highSound.play();
@@ -312,17 +360,21 @@ function updateArena(){
 	
 	//Pause game
 	if(Anigame.key && Anigame.key == 80 && started){
+		
+		//If paused, resume the game
 		if(paused){
 			paused = false;
 			Anigame.key = false;
-			//alert('Resuming game');
 		}
+		
+		//If running, pause the game
 		else{
 			paused = true;
 			Anigame.key = false;
-			//alert('Pausing game');
 		}
 	}
+	
+	//If game hasn't started, wait for it to start because of pressing SPACEBAR
 	if(!started){
 		bg.update();
 		bg1.update();
@@ -335,6 +387,7 @@ function updateArena(){
 	}
 	
 }
+
 //Function to add sound
 function playSound(src){
 	this.sound = document.createElement("audio");
